57e62eb6a809d57f4869fd332ccce3adfc5b2e90
DenkovychOleh <praice07@gmail.com>
2023-11-17 20:05:12 +0200

    Refactoring done

--- src/test/java/telegrambotchatgpt/controllers/pages/admin/AdminPageControllerTest.java
+++ src/test/java/telegrambotchatgpt/controllers/pages/admin/AdminPageControllerTest.java
@@ -7,14 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import telegrambotchatgpt.TestBean;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
